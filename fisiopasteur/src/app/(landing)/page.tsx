1c0b6adffea8a97641d164e86c299f204a975574
andrewblockernst <andrewblockernst@gmail.com>
2025-11-27 18:41:28 -0300

    hola

---
+++ fisiopasteur/src/app/(landing)/page.tsx
@@ -0,0 +1,21 @@
+import Header from '@/componentes/landing/Header';
+import HeroSection from '@/componentes/landing/HeroSection';
+import FeaturesSection from '@/componentes/landing/FeaturesSection';
+import PricingSection from '@/componentes/landing/PricingSection';
+import FunctionalitiesSection from '@/componentes/landing/FunctionalitiesSection';
+import ClientsSection from '@/componentes/landing/ClientsSection';
+
+export default function LandingPage() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <HeroSection />
+      <FeaturesSection />
+      <FunctionalitiesSection />
+      <PricingSection />
+      <ClientsSection />
+    </div>
+  );
+}
+
+//MENEM LO HIZO
